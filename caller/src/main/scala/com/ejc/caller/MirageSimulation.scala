a79ee614071ba307faf115a3ce6aeec28e671aa3
Csaba Bejan <v-cbejan@expediagroup.com>
2020-04-12 20:34:21 +0200

    Updated rate

--- caller/src/main/scala/com/ejc/caller/MirageSimulation.scala
+++ caller/src/main/scala/com/ejc/caller/MirageSimulation.scala
@@ -2,18 +2,24 @@ package com.ejc.caller
 
 import io.gatling.core.scenario.Simulation
 import io.gatling.core.Predef._
-import io.gatling.http.Predef.http
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration._
 
 class MirageSimulation extends Simulation {
   val httpProtocol = http
     .baseUrl("http://localhost:8080")
 
+  val deleteAllScn = scenario("DeleteAll")
+    .exec(http("deleteAll")
+      .delete("/employee/deleteAll"))
+
   val scn = scenario("GetAllEmployees")
     .exec(http("getAll")
-      .get("/employee/list"))
-    .pause(1)
+      .get("/employee/listStatic"))
 
-  setUp(
-    scn.inject(atOnceUsers(1))
-  ).protocols(httpProtocol)
+  setUp {
+    scn.inject(constantUsersPerSec(10) during (10 seconds)).protocols(httpProtocol)
+    //scn.inject(atOnceUsers(1)).protocols(httpProtocol)
+  }
 }
